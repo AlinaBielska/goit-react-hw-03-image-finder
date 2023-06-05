bfe33ceec05d664864007dcbd2ed7acf430e63d9
AlinaBielska <alinkabelka5@gmail.com>
2023-06-05 19:07:46 +0200

    stworzyłam foldery które będą zawierać komponenty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div></div>
   );
 };
