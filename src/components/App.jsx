d124d88e79155865e46c0d37b8538e12c2fa1f05
AlinaBielska <alinkabelka5@gmail.com>
2023-06-10 19:13:46 +0200

    dodano komponent Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,7 @@ export class App extends Component {
       <div>
         <Searchbar onSubmitInput={this.onSubmitInput} />
         <ImageGallery />
+        <Button />
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,1 +1,12 @@
-import Button from './Button.module.css';
+import React, {Component} from 'react';
+import css from './Button.module.css';
+
+class Button extends Component {
+    render() {
+        return (
+            <button type="submit">Load more</button>
+        );
+    }
+};
+
+export default Button
