920be8927813446e87e84b1fccc35e4055d2b3f1
AlinaBielska <alinkabelka5@gmail.com>
2023-06-14 22:53:49 +0200

    dodalam komponent Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
+import * as basicLightbox from 'basiclightbox';
 import css from './Modal.module.css';
+
+class Modal extends Component {
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+};
+
+export default Modal
